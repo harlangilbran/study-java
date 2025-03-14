56d55860d6eb4970cd7658e2c76455fb12a40c22
Harlan Gilbran <167880900+harlangilbran@users.noreply.github.com>
2025-03-14 22:56:39 +0700

    first commit

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author harlan-gilbran
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+       
+        System.out.println("===== BIODATA DIRI ====");
+        System.out.println("Nama  \t: Deka alavarizi");
+        System.out.println("Nim  \t: 2522169");
+        System.out.println("Alamat \t: palembang");
+        System.out.println("No TELP\t: 089518797270");
+
+
+    }
+}
